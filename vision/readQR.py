2281f286f420d68154903cb1bc49fc3bedf18940
Ashlesh Gawande <agawande@memphis.edu>
2015-06-27 12:59:53 -0500

    identify qr code in an image using the contour of three blocks in the qr code

--- vision/readQR.py
+++ vision/readQR.py
@@ -1,6 +1,10 @@
 import cv2
+import numpy as np
+import time
 
+img = cv2.imread("qr.png")
 img = cv2.imread("qrcode_simple.png")
+#img = cv2.imread("qrcode.jpg")
 
 # convert to greyscale
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -8,18 +12,47 @@ img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # coverting to binary - threshold
 ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
 
+# Canny is an edge detection algorithm, http://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_imgproc/py_canny/py_canny.html
+canny=cv2.Canny(thresh1, 100, 200)
+
 # find contours, http://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_imgproc/py_contours/py_contours_begin/py_contours_begin.html
-image, contours, hierarchy = cv2.findContours(thresh1,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
+image, contours, hierarchy = cv2.findContours(canny,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 #RETR_TREE: read heirarchies
 
+print hierarchy
+
+qrSquares = []
 count=0
 for cnt in contours:
-    perimeter = cv2.arcLength(cnt,True)
-    print cnt
-    print(perimeter)
+    if count+5 == len(contours):
+        break
+
+    # need to find those contours which are five level deep to identify the three squares on the qrcode
+
+    # 1st = 0 from numpy, count=contour number, 2=child of the contour
+    child = hierarchy[0][count][2]
+    gchild = hierarchy[0][count+1][2]
+    g2child = hierarchy[0][count+2][2]
+    g3child = hierarchy[0][count+3][2]
+    g4child = hierarchy[0][count+4][2]
+    g5child = hierarchy[0][count+5][2]
+
+    # make sure that fifth contour is childless
+    if g5child == -1:
+        # if child hierarchy is legit
+        if child+1 == gchild and gchild+1 == g2child and g2child+1 == g3child and g3child+1 == g4child:
+            print child
+            print gchild
+            print g2child
+            print g3child
+            print g4child
+            print g5child
+            qrSquares.append(cnt)
     count+=1
 
-print(count)
+cv2.drawContours( image, qrSquares, -1, (255, 255, 255), 3 )
+
+#print(count)
 
 cv2.imshow('squares', image)
 
